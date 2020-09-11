1c11d107e795e5f40393532facb62a4924779552
WithWings889 <ironwoman889@gmail.com>
2020-09-11 20:58:24 +0300

    Find words with repetition

---
+++ src/Lab1.java
@@ -0,0 +1,70 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Lab1 {
+    private static int counter = 0;
+    private static boolean isWithoutRepetitions = true;
+    private static ArrayList<String> selectedWords = new ArrayList<>();
+
+    public static void clearWord(char[] word) {
+        for(int i = 0; i < counter; ++i)
+            word[i] = ' ';
+    }
+    public static void readFile() {
+        File file = new File("src\\text");
+        try {
+            Scanner scanner = new Scanner(file);
+            while(scanner.hasNextLine()){
+                String line = scanner.nextLine() + " ";
+                char[] word = new char[30];
+                for(int i = 0; i < line.length(); ++i){
+                    boolean isNotLetter = ((int)line.charAt(i) < 97 && (int)line.charAt(i) > 90) || (int)line.charAt(i) < 65 || (int)line.charAt(i) > 122 ;
+
+                    if (counter >= 30 && !isNotLetter)
+                        continue;
+                    else{
+                        if (isNotLetter && counter != 0){
+                            if(isWithoutRepetitions) {
+                                String string = new String(word);
+                                for(String w: selectedWords)
+                                    if( w == string)
+                                        continue;
+                                selectedWords.add(string);
+                            }
+                            clearWord(word);
+                            counter = 0;
+                            isWithoutRepetitions = true;
+                        }
+                        else if(isNotLetter && counter == 0)
+                            continue;
+                        else {
+                            if (isWithoutRepetitions)
+                                for(int j = 0; j < counter; ++j)
+                                    if(word[j] == line.charAt(i)) {
+                                        isWithoutRepetitions = false;
+                                        break;
+                                    }
+                            word[counter] = line.charAt(i);
+                            ++counter;
+
+                        }
+                    }
+
+                }
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File is not found");
+        }
+    }
+
+    public static void main(String[] args){
+        readFile();
+
+        for(String word: selectedWords){
+            System.out.println(word);
+        }
+    }
+}
